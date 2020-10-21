27a0537d8d84b5e3a198c7adc3d9e6041f06feba
Kevin Traini <kevin@ktraini.com>
2020-10-21 07:58:27 +0200

    Question 1

--- main.c
+++ main.c
@@ -9,3 +9,19 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+
+
+
+
+// _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.
+/**
+ * QUESTIONS
+ *
+ * Question 1 :
+ * 1) La commande $ echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaa | grep -E "^(a?){30}a{30}$" ne renvoie rien
+ * 2) Par contre, si on met 30 'a' dans echo (au lieu de 29), la commande nous renvoie les 30 'a'
+ *
+ */
+
+
